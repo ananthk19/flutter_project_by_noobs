93ac377ba554f4f5a1cb0a85dd7d04e89fb598d8
Anand <ananthakrishnan2019@iiitkottayam.ac.in>
2022-02-22 12:47:49 +0530

    MapBox Setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'dart:collection';
 
+import 'package:latlong2/latlong.dart';
+import 'package:flutter_map/flutter_map.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_database/firebase_database.dart';
@@ -15,7 +17,7 @@ void main() async {
       title: 'flutter_project_by_noobs',
       initialRoute: '/',
       routes: {
-        '/': (context) =>  customer_login(),
+        '/': (context) =>  NavigationRoute(),
         '/customer': (context) => const CustomerRoute(),
         '/myAgents': (context) => const MyAgentsRoute(),
         '/agent': (context) => const AgentRoute(),
@@ -36,28 +38,54 @@ class NavigationRoute extends StatelessWidget {
       appBar: AppBar(
         title: const Text("Navigation"),
       ),
-      body: Column(
-        children: [
-          ElevatedButton(
-            child: const Text("to Customer"),
-            onPressed: () {
-              Navigator.pushNamed(context, "/customer");
-            },
+      // body: Column(
+      //   children: [
+          
+      //     ElevatedButton(
+      //       child: const Text("to Customer"),
+      //       onPressed: () {
+      //         Navigator.pushNamed(context, "/customer");
+      //       },
+      //     ),
+      //      ElevatedButton(
+      //       child: const Text("To Agent"),
+      //       onPressed: (){
+      //         Navigator.pushNamed(context, '/agent');
+      //       },
+      //      ),
+      //   ],
+      // ),
+      body: FlutterMap(
+            options: MapOptions(
+              center: LatLng(51.5, -0.09),
+              zoom: 13.0,
+            ),
+            layers: [
+              TileLayerOptions(
+                urlTemplate: "https://api.mapbox.com/styles/v1/knight555/ckzxpi6wn00d415p7ai97wesv/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1Ijoia25pZ2h0NTU1IiwiYSI6ImNrendtcHh0djAwZnEyd3M0OHZ3N3dtanUifQ.2fffqnLOGICTz37Ot15wdw",
+                additionalOptions: {
+                  'accessToken': "pk.eyJ1Ijoia25pZ2h0NTU1IiwiYSI6ImNrendtcHh0djAwZnEyd3M0OHZ3N3dtanUifQ.2fffqnLOGICTz37Ot15wdw",
+                  'id': "mapbox.mapbox-streets-v8",
+                },
+                attributionBuilder: (_) {
+                  return Text("© OpenStreetMap contributors");
+                },
+              ),
+              MarkerLayerOptions(
+                markers: [
+                  Marker(
+                    width: 80.0,
+                    height: 80.0,
+                    point: LatLng(51.5, -0.09),
+                    builder: (ctx) =>
+                    Container(
+                      child: Icon(Icons.home),
+                    ),
+                  ),
+                ],
+              ),
+            ],
           ),
-           ElevatedButton(
-            child: const Text("To Agent"),
-            onPressed: (){
-              Navigator.pushNamed(context, '/agent');
-            },
-           ),
-          ElevatedButton(
-            child: const Text("Test"),
-            onPressed: (){
-
-            },
-          ),
-        ],
-      ),
     );
   }
 }
